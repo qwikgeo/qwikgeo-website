e40ef353231760d4406af7ab14c2750c7ae2f182
mkeller3 <michaelkeller03@gmail.com>
2022-10-03 07:56:23 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="/qwikgeo.png"
+          transition="scale-transition"
+          width="60"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
