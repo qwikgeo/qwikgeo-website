281abd52573bc86bbae5fd3bd2deb10e6c820046
mkeller3 <michaelkeller03@gmail.com>
2022-10-04 11:48:20 -0500

    Updates for homepage

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
+import '@/styles/variables.scss'
+import '@/styles/styles.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,53 @@
 <template>
   <v-container fluid class="mx-0 px-0 pt-0 h-100">
-    <v-row class="mx-0 px-0 mt-0 pt-0 h-100">
-      <v-col class="mx-0 px-0 mt-0 pt-0 hero-banner">
+    <v-row class="mx-0 px-0 mt-0 pt-0">
+      <v-col cols="12" class="mx-0 px-0 mt-0 pt-0 hero-banner">
         <v-img
           height="100%"
           src="/images/home_banner_2.jpg"
-          gradient="rgba(0, 0, 0, .42), rgba(0, 0, 0, .40)"
+          gradient="rgba(0, 0, 0, .52), rgba(0, 0, 0, .52)"
         >
         <v-layout
-          fill-height
           align-center
-          pa-3
+          px-3
+          pt-10
         >
           <v-flex
             xs12
             md10
-            offset-md2
+            pt-10
           >
-            <h1 class="display-3 font-weight-light white-text">
+            <h1 class="display-3 font-weight-light white-text pt-10">
               QwikGeo
             </h1>
-            <div class="display-7 text-uppercase pl-2 mb-4 mt-2 white-text">
+            <h5 class="text-uppercase pl-2 mb-4 mt-2 white-text">
               A state of the art geospatial platform for Location Services and GeoAnalytics.
-            </div>
+            </h5>
           </v-flex>
         </v-layout>
+        </v-img>
+      </v-col>
+    </v-row>
+    <!-- <v-row class="mx-0 px-0 mt-0">
+      <v-col cols="8" class="text-center pt-6">
+        <v-card class="pa-4 rounded-xl" elevation="20">
+          <v-icon
+            x-large
+            style="font-size: 8rem"
+          >
+            mdi-database
+          </v-icon>
+        </v-card>
+      </v-col>
+    </v-row> -->
+
+    <v-row class="mx-0 px-0 mt-0">
+      <v-col cols="12" md="8" class="text-center pt-3">
+        <div>QwikGeo makes it easy for you to answer where and why things are happening.</div>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-img src="/images/home_3.jpg" height="300">
+
         </v-img>
       </v-col>
     </v-row>
@@ -39,16 +62,8 @@ export default {
 
 <style>
 
-.white-text {
-  color: #fff !important;
-}
-
 .hero-banner{
   height: 50vh;
-}
-
-.h-100 {
-  height: 100%;
 }
 
 @media only screen and (max-width: 600px) {
